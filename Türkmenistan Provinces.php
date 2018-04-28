90a80302adda932d7b67fdfe20b78eeb54387248
bayramgeldi <bayramgeldi.bayriyev@gmail.com>
2018-04-28 18:05:24 +0300

    PHP array of Turkmenistan Provinces and Districts
    PHP array of Turkmenistan Provinces and Districts

---
+++ Türkmenistan Provinces.php
@@ -0,0 +1,91 @@
+<?php
+$turkmenistan=array(
+  'Aşgabat' => array(
+      'Aşgabat'
+                    )
+
+  'Ahal' => array(
+    'Änew',
+    'Abadan',
+    'Akbugdaý',
+    'Altyn Asyr',
+    'Babadaýhan',
+    'Baharly',
+    'Gökdepe',
+    'Kaka',
+    'Derweze',
+    'Sarahs',
+    'Tejen'
+  ),
+
+  'Mary' => array(
+    'Baýramaly',
+    'Mary',
+    'Altyn Sähra',
+    'Baýramaly',
+    'Ýolöten',
+    'Garagum',
+    'Murgap',
+    'Oguzhan',
+    'Sakarçäge',
+    'Serhetabat',
+    'Tagtabazar',
+    'Türkmengala',
+    'Wekilbazar'
+  ),
+
+  'Daşoguz'=>array(
+    'Daşoguz',
+    'Köneürgenç',
+    'Akdepe',
+    'Boldumsaz',
+    'Görogly',
+    'Gubadag',
+    'Gurbansoltan Eje',
+    'Ruhubelent',
+    'Saparmyrat Nyýazow',
+    'Saparmyrat Türkmenbaşy'
+  ),
+
+  'Balkan'=>array(
+    'Bereket',
+    'Esenguly',
+    'Etrek',
+    'Magtymguly',
+    'Serdar',
+    'Türkmenbaşy',
+    'Balkanabat',
+    'Garabogaz',
+    'Gumdag',
+    'Hazar',
+    'Serdar'
+  ),
+
+  'Lebap'=>array(
+    'Çärjew',
+    'Dänew',
+    'Birganata',
+    'Döwletli',
+    'Farap',
+    'Halaç',
+    'Hojambaz',
+    'Köýtendag',
+    'Saýat',
+    'Seýdi',
+    'Kerki',
+    'Magdanly',
+    'Seýdi',
+    'Türkmenabat'
+  )
+    );
+
+    $provinces = array(
+        'Aşgabat',
+        'Mary',
+        'Ahal',
+        'Lebap',
+        'Daşoguz',
+        'Balkan'
+    );
+
+  ?>
